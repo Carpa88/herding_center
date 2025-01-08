bbbc08e02ece1c5bd86547cbb09ee4940e8cb2e5
Carpa88 <olenka_311@mail.ru>
2025-01-08 14:13:38 +0100

    added Visual and Contacts sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,23 @@
 import AboutUs from "./ui/main-sections/AboutUs";
+import ContactsSection from "./ui/main-sections/ContactsSection";
 import Features from "./ui/main-sections/Features";
 import HeroSection from "./ui/main-sections/HeroSection";
 import PricingSection from "./ui/main-sections/PricingSection";
 import Testimonials from "./ui/main-sections/Testimonials";
+import VisualsSection from "./ui/main-sections/VisualsSection";
 import PageCover from "./ui/PageCover";
 
 
 const Page = () => {
   return (
-    <PageCover title='Пастухи Беларуси'>      
+    <PageCover title='Пастухи Беларуси'>
       <HeroSection />
       <Features />
       <Testimonials />
       <AboutUs />
       <PricingSection />
+      <VisualsSection />
+      <ContactsSection />
     </PageCover>
   );
 }
