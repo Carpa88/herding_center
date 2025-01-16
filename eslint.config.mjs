fd5eafca431e4dd0ca1a58eeca517e02c3aa92a5
Carpa88 <olenka_311@mail.ru>
2025-01-16 22:40:20 +0100

    added code styles

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,8 @@
+import reactPlugin from 'eslint-plugin-react';
+import jsxA11yPlugin from 'eslint-plugin-jsx-a11y';
+import typescriptPlugin from '@typescript-eslint/eslint-plugin';
+import prettierPlugin from 'eslint-plugin-prettier';
+
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -11,6 +16,46 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    ignores: ['node_modules', 'dist'], // Игнорировать эти папки
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      react: reactPlugin,
+      'jsx-a11y': jsxA11yPlugin,
+      '@typescript-eslint': typescriptPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'prettier/prettier': 'error',
+      'jsx-a11y/alt-text': 'error',
+      'jsx-a11y/no-autofocus': 'warn',
+      'arrow-body-style': ['error', 'as-needed'],
+      'consistent-return': 'error',
+      'prefer-const': 'error',
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always'],
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
 ];
 
 export default eslintConfig;
