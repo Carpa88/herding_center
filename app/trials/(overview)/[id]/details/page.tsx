98299cdcb95241c9bc36042bef966c3defaa1b45
Carpa88 <olenka_311@mail.ru>
2025-01-19 17:18:04 +0100

    parsed api for details page

--- app/trials/(overview)/[id]/details/page.tsx
+++ app/trials/(overview)/[id]/details/page.tsx
@@ -1,5 +1,3 @@
-'use server';
-
 import PageCover from '@app/_ui/PageCover';
 import { fetchTrial } from '../../actions';
 import { format } from 'date-fns';
@@ -16,21 +14,28 @@ const Item = ({ name, value }: { name: string; value: string }) => (
 const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const trialID = (await params).id;
   const trial = await fetchTrial(trialID);
-  const dateCreated = format(new Date(trial[0].created_at), 'yyyy-MM-dd ');
+  const data = trial.data;
+
+  if (!data) {
+    return <p>{trial.message}</p>;
+  }
+  const dateCreated = data.created_at
+    ? format(new Date(data.created_at), 'yyyy-MM-dd')
+    : 'Дата неизвестна';
 
   return (
-    <PageCover title={trial[0].name}>
+    <PageCover title={data.name}>
       <div>
         <p className="mt-1 max-w-2xl text-sm/6 text-slate-500">
-          {trial[0].description}
+          {trial.data?.description}
         </p>
         <div className="mt-6 border-t border-slate-100">
           <dl className="divide-y divide-slate-100">
             <Item
               name="Время проведения соревнований:"
-              value={`${trial[0].start_at} - ${trial[0].ends_on}`}
+              value={`${data.start_at} - ${data.ends_on}`}
             />
-            <Item name="Судья:" value={trial[0].judge_id} />
+            <Item name="Судья:" value={data.judge_id} />
             <Item name="Дата создания записи:" value={dateCreated} />
           </dl>
         </div>
--- app/trials/(overview)/actions.ts
+++ app/trials/(overview)/actions.ts
@@ -1,10 +1,12 @@
 'use server';
 
 import { sql } from '@vercel/postgres';
-import { ITrial } from '../types';
+import { ITrial, ITrialError } from '../types';
 import { API_BASE_URL } from '@app/_lib/consts';
 import { IResponseData } from '@app/_lib/types';
 import { ERROR_MES_REQUEST } from '../consts';
+import { redirect } from '@node_modules/next/navigation';
+import { revalidatePath } from 'next/cache';
 
 export const fetchTrialsPages = async (
   query: string,
@@ -79,10 +81,63 @@ export const deleteTrial = async (id: string) => {
 
 export const fetchTrial = async (id: string) => {
   try {
-    const result = await sql`SELECT * FROM trials WHERE id = ${id}`;
-    return result.rows;
+    const response = await fetch(`${API_BASE_URL}/trials/details`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+        id,
+      },
+    });
+    if (!response.ok) {
+      const { message, error } = await response.json();
+      return {
+        error: error as Error,
+        message: message as string,
+        data: null,
+      };
+    }
+
+    const request: IResponseData<ITrial, string> = await response.json();
+    return { error: '', message: '', data: request.data };
   } catch (error) {
-    console.error('offset:', error);
-    throw new Error('Ошибка соединения с базой данных');
+    console.error('Fetch error:', error);
+    return { error: error as Error, message: ERROR_MES_REQUEST, data: null };
   }
+};
+
+export const updateTrial = async (
+  state: IResponseData<ITrial, ITrialError>,
+  formData: FormData,
+): Promise<IResponseData<ITrial, ITrialError>> => {
+  const body = {
+    name: formData.get('name'),
+    start_at: formData.get('start_at'),
+    ends_on: formData.get('ends_on'),
+    judge_id: formData.get('judge_id'),
+    description: formData.get('description'),
+  };
+
+  try {
+    const response = await fetch(`${API_BASE_URL}/trials/update`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+        data: JSON.stringify(body),
+      },
+    });
+
+    if (!response.ok) {
+      const { message, error } = await response.json();
+      return {
+        error: error as Error,
+        message: message as string,
+        data: null,
+      };
+    }
+  } catch (error) {
+    console.error('Fetch error:', error);
+    return { error: error as Error, message: ERROR_MES_REQUEST, data: null };
+  }
+  revalidatePath('/trials');
+  redirect('/trials');
 };
