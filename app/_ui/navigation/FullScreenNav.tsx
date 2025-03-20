8468a3ed4aeabd492639daf776fb8ab496acd211
Carpa88 <olenka_311@mail.ru>
2025-03-20 11:18:28 +0100

    Correct styles

--- app/_ui/navigation/FullScreenNav.tsx
+++ app/_ui/navigation/FullScreenNav.tsx
@@ -5,15 +5,15 @@ import Link from 'next/link';
 const FullScreenNav = ({ pathname }: { pathname: string }) => (
   <div className="hidden md:block">
     <div className="ml-10 flex items-baseline space-x-4">
-      {navigation.map((item) => (
+      {navigation.map(item => (
         <Link
           key={item.name}
           href={item.href}
           aria-current={item.href === pathname ? 'page' : undefined}
           className={clsx(
             item.href === pathname
-              ? 'border-b-4 border-amber-900 text-white'
-              : 'text-slate-100 hover:bg-amber-700 hover:text-white',
+              ? 'border-b-2 border-orange-800 text-white rounded-none'
+              : 'text-slate-100 hover:bg-orange-700 hover:text-white',
             'rounded-md px-3 py-2 text-sm font-medium',
           )}
         >
