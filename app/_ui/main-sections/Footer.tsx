66df09ea1cb0b829494005caa2692aa3f9ef2c10
Carpa88 <olenka_311@mail.ru>
2025-03-20 12:30:38 +0100

    added color variables

--- app/_ui/main-sections/Footer.tsx
+++ app/_ui/main-sections/Footer.tsx
@@ -23,15 +23,15 @@ const FooterBlock = ({ data }: { data: Record<string, string> }) => (
 );
 
 const Footer = () => (
-  <div className="bg-amber-800 " id="footer">
+  <div className="bg-bgPrimary" id="footer">
     <div className="mx-auto max-w-7xl px-6 lg:px-8">
-      <div className="grid gap-20 grid-cols-1 md:grid-cols-4 lg:grid-cols-6 md:pt-8 md:px-8 p-6 text-white overflow-hidden">
+      <div className="grid gap-20 grid-cols-1 md:grid-cols-4 lg:grid-cols-6 md:pt-8 md:px-8 p-6 text-textDefault overflow-hidden">
         <div className="md:col-span-4 lg:col-span-2">
           <Logo />
           <p className="py-2 md:py-6">Ваша собака отдыхает, пока вы заняты.</p>
           <div>
             <ul className="flex flex-row">
-              {socials.map((item) => (
+              {socials.map(item => (
                 <li className="mr-4 text-3xl" key={item.id}>
                   {<item.icon />}
                 </li>
@@ -43,7 +43,7 @@ const Footer = () => (
         <FooterBlock data={footer.hostel} />
         <FooterBlock data={footer.competitions} />
         <FooterBlock data={footer.practicing} />
-        <div className="md:col-span-4 lg:col-span-6 border-t-[1px] pt-4 border-slate-300">
+        <div className="md:col-span-4 lg:col-span-6 border-t-[1px] pt-4 border-borderLight">
           <p>Copyright, 2025</p>
         </div>
       </div>
--- app/_ui/main-sections/HeroSection.tsx
+++ app/_ui/main-sections/HeroSection.tsx
@@ -7,30 +7,33 @@ const HeroSection = () => (
       <div className="mx-auto flex flex-row max-w-2xl sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2 h-[600px]">
         <div className="lg:pr-8 lg:pt-4 md:w-[50%] sm:w-[100%] h-[100%] content-center md:pr-8">
           <div className="lg:max-w-lg">
-            <div className="relative rounded-full px-3 py-1 text-sm/6 text-slate-600 ring-1 ring-slate-900/10 hover:ring-slate-900/20">
+            <div className="relative rounded-full px-3 py-1 text-sm/6 text-textSecondary ring-1 ring-slate-900/10 hover:ring-slate-900/20">
               Новый стандарт заботы о вашем питомце.{' '}
-              <Link href="#" className="font-semibold text-amber-600">
+              <Link href="#" className="font-semibold text-textDisabled">
                 <span aria-hidden="true" className="absolute inset-0" />
                 Узнайте больше <span aria-hidden="true">&rarr;</span>
               </Link>
             </div>
           </div>
           <div className="text-center">
-            <h1 className="mt-8 text-pretty text-4xl font-semibold tracking-tight text-slate-900 sm:text-5xl">
+            <h1 className="mt-8 text-pretty text-4xl font-semibold tracking-tight text-textPrimary sm:text-5xl">
               Уютная гостиница для ваших собак
             </h1>
-            <p className="mt-8 text-pretty text-lg font-medium text-slate-500 sm:text-xl/8">
+            <p className="mt-8 text-pretty text-lg font-medium text-textSecondary sm:text-xl/8">
               Заботимся о вашем питомце, как о своем. Комфортные условия,
               круглосуточный уход и индивидуальный подход.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <Link
                 href="#"
-                className="rounded-md bg-amber-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-amber-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-amber-600"
+                className="rounded-md bg-buttonDefault px-3.5 py-2.5 text-sm font-semibold text-textDefault shadow-sm hover:bg-buttonHover focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-buttonActive"
               >
                 Забронировать место
               </Link>
-              <Link href="#" className="text-sm/6 font-semibold text-slate-900">
+              <Link
+                href="#"
+                className="text-sm/6 font-semibold text-textPrimary"
+              >
                 Узнайте больше <span aria-hidden="true">→</span>
               </Link>
             </div>
@@ -38,7 +41,7 @@ const HeroSection = () => (
         </div>
         <div className="hidden md:block w-[50%]">
           <div className="relative min-h-[40rem] w-full grow [container-type:inline-size] max-lg:mx-auto max-lg:max-w-sm">
-            <div className="absolute inset-x-5 bottom-0 top-10 overflow-hidden rounded-[4cqw] bg-slate-900 shadow-xl">
+            <div className="absolute inset-x-5 bottom-0 top-10 overflow-hidden rounded-[4cqw] bg-bgPrimary shadow-xl">
               <Image
                 width={500}
                 height={500}
