24a5f1b4a450a9698b50a7ffe216c0adc85f816c
Carpa88 <olenka_311@mail.ru>
2025-03-26 13:05:48 +0100

    added pet folder

---
+++ app/pet/[id]/page.tsx
@@ -0,0 +1,10 @@
+import PageCover from '@app/_ui/PageCover';
+import React from 'react';
+
+const Pet = () => (
+  <PageCover title="Ваш питомец">
+    <div>Pet page</div>
+  </PageCover>
+);
+
+export default Pet;
