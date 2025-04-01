c1011695e25e1cbe1c76052343ae542047e3f616
Carpa88 <olenka_311@mail.ru>
2025-04-01 13:43:07 +0200

    added singin after singup

--- app/(user)/singup/page.tsx
+++ app/(user)/singup/page.tsx
@@ -5,22 +5,45 @@ import Section from '@app/_ui/form/Section';
 import PageCover from '@app/_ui/PageCover';
 import Input from '@app/_ui/form/Input';
 import { initialState } from '@app/_lib/consts';
-import { useActionState } from 'react';
+import { useActionState, useEffect, useState } from 'react';
 import { signup } from '../action';
 import { ISinginError } from '../types';
+import { signIn } from '@node_modules/next-auth/react';
+import { redirect } from '@node_modules/next/navigation';
 
 const SingUp = () => {
   const [state, formAction, isPading] = useActionState(signup, initialState);
+  const [messageError, setMessageError] = useState('');
+
+  useEffect(() => {
+    const doSignIn = async () => {
+      if (state.data) {
+        const result = await signIn('credentials', {
+          email: state.data.email,
+          password: state.data.password,
+        });
+
+        if (result?.ok) {
+          redirect('/profile');
+        } else {
+          setMessageError(result?.error || '');
+        }
+      }
+    };
+    doSignIn();
+  }, [state]);
+
   const errorObj = state.error as Partial<ISinginError>;
+
   return (
     <PageCover title="Пожалуйста, войдите в систему">
       <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4">
         <Form
           buttonState={isPading}
           formAction={formAction}
-          buttonName="Войти в систему"
+          buttonName="Зарегистрироваться"
           buttonClass="w-full"
-          errorMessage={state.message}
+          errorMessage={state.message || messageError}
         >
           <Section cols={1}>
             <Input
