db547970a532b7b7cdc18bf5ebe900f325a6dce9
Carpa88 <olenka_311@mail.ru>
2025-04-04 10:12:52 +0200

    recode PetList.tsx

--- app/(user)/profile/PetList.tsx
+++ app/(user)/profile/PetList.tsx
@@ -1,11 +1,9 @@
 'use client';
 
+import { IMAGE } from '@app/pet/consts';
 import { IDog } from '@app/pet/types';
 import Link from '@node_modules/next/link';
 import Image from 'next/image';
-
-const IMAGE =
-  'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80';
 
 const PetList = ({ data }: { data: IDog[] }) => (
   <div className="mx-auto w-full ">
--- app/pet/consts.ts
+++ app/pet/consts.ts
@@ -13,3 +13,6 @@ export const PET_SEX = [
     value: 'female',
   },
 ];
+
+export const IMAGE =
+  'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80';
