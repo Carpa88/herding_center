ee535af25d5af284dae41df7a56382f1a54186d5
Carpa88 <olenka_311@mail.ru>
2025-04-10 12:28:00 +0200

    added buttons for Title for pages

--- app/_ui/PageCover.tsx
+++ app/_ui/PageCover.tsx
@@ -4,12 +4,16 @@ import Title from './Title';
 const PageCover = ({
   children,
   title,
+  href,
+  name,
 }: {
   children: React.ReactNode;
   title: string;
+  href?: string;
+  name?: string;
 }) => (
   <>
-    <Title title={title} />
+    <Title title={title} href={href} name={name} />
     <main>
       <div className="mx-auto max-w-7xl px-4 py-0 sm:px-6 lg:px-8">
         <div className="bg-white py-6 px-4">{children}</div>
--- app/_ui/Title.tsx
+++ app/_ui/Title.tsx
@@ -1,9 +1,29 @@
-const Title = ({ title }: { title: string }) => (
+import { PlusIcon } from '@node_modules/@heroicons/react/24/outline';
+import Link from '@node_modules/next/link';
+
+const Title = ({
+  title,
+  href,
+  name,
+}: {
+  title: string;
+  href?: string;
+  name?: string;
+}) => (
   <header className="relative z-10 bg-bgDefault shadow">
-    <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+    <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 flex justify-between">
       <h1 className={'text-3xl font-bold tracking-tight text-textPrimary'}>
         {title}
       </h1>
+      {href && (
+        <Link
+          href={href}
+          className="flex h-10 items-center rounded-lg bg-buttonDefault px-4 text-sm font-medium text-textDefault transition-colors hover:bg-buttonHover focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-buttonActive"
+        >
+          <span className="hidden md:block">{name}</span>{' '}
+          <PlusIcon className="h-5 md:ml-4" />
+        </Link>
+      )}
     </div>
   </header>
 );
