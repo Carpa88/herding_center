fd53d2a620db94c6a8570664079c64d92ae1dfa7
Carpa88 <olenka_311@mail.ru>
2025-04-16 15:52:41 +0200

    added condition for button

--- app/trials/[id]/page.tsx
+++ app/trials/[id]/page.tsx
@@ -3,8 +3,11 @@ import { fetchTrial, getLastTrial } from '@app/trials/actions';
 import { format } from 'date-fns';
 import { Item } from '@app/_ui/Item';
 import { ParamsType } from '@app/_lib/types';
+import { getServerSession } from 'next-auth';
+import { authConfig } from '@app/_configs/auth';
 
 const TrialPage = async ({ params }: ParamsType) => {
+  const session = await getServerSession(authConfig);
   const trialID = (await params).id;
   const trial = await fetchTrial(trialID);
   const lastTrial = await getLastTrial();
@@ -18,11 +21,32 @@ const TrialPage = async ({ params }: ParamsType) => {
     : 'Дата неизвестна';
 
   const isLast = trial?.data?.id === lastTrial?.data?.id;
+
+  const rightButton = () => {
+    const props: { href?: string; name?: string; plus?: boolean } = {};
+    if (session?.user?.role === 'admin' && isLast) {
+      props.href = `/trials/${trialID}/participants`;
+      props.name = 'Список участников';
+      props.plus = false;
+    } else if (session?.user?.role === 'user' && isLast) {
+      props.href = `/trials/${trialID}/app`;
+      props.name = 'Записаться на соревнование';
+      props.plus = true;
+    } else {
+      props.href = '';
+      props.name = '';
+      props.plus = true;
+    }
+    return props;
+  };
+  const but = rightButton();
+
   return (
     <PageCover
       title={data.name}
-      href={isLast ? `/trials/${trialID}/app` : ''}
-      name={isLast ? 'Записаться на соревнование' : ''}
+      href={but.href}
+      name={but.name}
+      plus={but.plus}
     >
       <div>
         <p className="mt-1 max-w-2xl text-sm/6 text-slate-500">
